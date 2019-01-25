0b117f42dc1ee47223159631d4ba6d6a33215a5c
Ecker Quirin <it170135@EDU.HTL-LEONDING.AC.AT>
2019-01-25 12:36:31 +0100

    character Control in progress

--- scripts/characterControl.js
+++ scripts/characterControl.js
@@ -1,5 +1,16 @@
-window.addEventListener("keydown", () => {
-  if (true) {
+let characterCoordinates = {
+  x: 960,
+  y: 540
+}
 
+window.addEventListener('keydown', (e) => {
+  let characterObject = document.getElementById("character");
+
+  if (e.key === 'd') {
+    characterCoordinates.x = characterControl.x + 1;
+  } else if (e.key === 'a') {
+    characterCoordinates.x = characterControl.x - 1;
   }
+
+  alert(characterCoordinates.x + characterControl.y);
 });
